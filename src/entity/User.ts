16bc4048bf5cd9d1054c14276dbbc504c7725224
Arthur <arthurldh@gmail.com>
2023-04-27 10:32:02 +0200

    chors: first entities

---
+++ src/entity/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    username: string
+
+    @Column()
+    avatar: string
+}

1e68d7cad443c0fa6f1491823215de7be05104a6
EONO Logan <eonologan@gmail.com>
2023-06-26 14:20:09 +0200

    move chipzone in playerzone

--- src_front/src/components/chipZone/ChipZone.tsx
+++ src_front/src/components/chipZone/ChipZone.tsx
@@ -2,28 +2,16 @@ import React from 'react';
 import './ChipZone.css';
 import Chip from '../chip/Chip';
 
-interface Player {
-    id: number;
-    pseudo: string;
-}
-
-interface ChipZoneProps {
-    players: Player[];
-}
-
-const ChipZone: React.FC<ChipZoneProps> = ({ players }) => {
+const ChipZone = () => {
     return (
         <div className="chip-zone">
-            {players.map((player, index) => (
-                <div className="chip-container" key={player.id}>
-                    <h3>{player.pseudo}</h3>
-                    <div>
-                        <Chip value={50} color="red" />
-                        <Chip value={100} color="green" />
-                        <Chip value={500} color="blue" />
-                    </div>
+            <div className="chip-container">
+                <div>
+                    <Chip value={50} color="red"/>
+                    <Chip value={100} color="green"/>
+                    <Chip value={500} color="blue"/>
                 </div>
-            ))}
+            </div>
         </div>
     );
 };
--- src_front/src/components/game/Game.tsx
+++ src_front/src/components/game/Game.tsx
@@ -7,32 +7,24 @@ import RuleZone from "../ruleZone/RuleZone";
 import PlayerZone from "../playerZone/PlayerZone";
 import BetZone from '../betZone/BetZone';
 
-// Récupération du noms/ids des joueurs passé en props de ChipZone
-const players = [
-    { id: 1, 'pseudo': 'The-Cyril5555' },
-    { id: 2, 'pseudo': 'Neksooo' },
-    { id: 3, 'pseudo': 'Senshi' },
-];
-const playerNames = ["The-Jules6666", "Pierre", "Paul", "Jacques", "Sophie", "Marie", "Louise"];
+const playerNames = ["The-Jules6666", "Pierre", "Paul", "Jacques", "Sophie", "Marie"];
 // Récupération du status du joueur passé en props de PlayerZone
 const playerStatuses = ["not-played", "not-played", "not-played", "playing", "played", "played", "played"]; // Exemple de statuts
 // Récupération des valeurs des cartes
 const cardValues = [10, 20, 15, 0, 18, 16, 20];
 
 const Game = () => {
-    return(
-            <div className="App" >
-                <header className="App-header">
-                    <DealerZone />
-                    <DiscardPileZone />
-
-                    <ChipZone players={players}/>
-                    <PlayerZone playerNames={playerNames} playerStatuses={playerStatuses} cardValues={cardValues}/>
-                    <BetZone minValue={2} />
-                    <RuleZone />
-                </header>
-            </div>
-        )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <DealerZone/>
+                <DiscardPileZone/>
+                <PlayerZone playerNames={playerNames} playerStatuses={playerStatuses} cardValues={cardValues}/>
+                <BetZone minValue={2}/>
+                <RuleZone/>
+            </header>
+        </div>
+    )
 }
 
 export default Game;

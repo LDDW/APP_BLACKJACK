2e97c7d8ceab741a0ebf1a2b61580d736197e75d
nicolas <nicolas.tual@outlook.fr>
2023-06-26 09:37:48 +0200

    Add Navigator

---
+++ mobile/src/navigator/stack.navigator.tsx
@@ -0,0 +1,38 @@
+import {createNativeStackNavigator, NativeStackScreenProps} from "@react-navigation/native-stack";
+import ChatScreen from "../screens/chat/chat.screen";
+
+/**
+ * Navigateur React Native
+ * Voir la doc : https://reactnavigation.org/docs/native-stack-navigator
+ * AVANT DE DÉCLARER UN SCREEN DANS LE STACK :
+ * - Ajouter le nom du screen dans le type IRootParamsList avec la valeur "undefined"
+ * - Ca permet d'accéder à l'instance de navigation dans chaque screen
+ * - Un exemple de screen pour naviguer entre deux screens :
+ * 			const ChatScreen = ({ navigation }: IRootStackProps<'Chat'>) => {
+ * 				return (
+ * 					<Pressable onPress={() => navigation.navigate('Home')}>
+ *
+ *					</Pressable>
+ * 				)
+ * 			}
+ */
+
+type IRootParamsList = {
+	// Ajouter ici le nom des screens
+	Chat: undefined
+}
+
+const Stack = createNativeStackNavigator<IRootParamsList>();
+
+const StackNavigator = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Group>
+				<Stack.Screen name="Chat" component={ChatScreen}/>
+			</Stack.Group>
+		</Stack.Navigator>
+	)
+}
+
+export default StackNavigator
+export type IRootStackProps<T extends keyof IRootParamsList> = NativeStackScreenProps<IRootParamsList, T>;
---
+++ mobile/src/screens/chat/chat.screen.tsx
@@ -0,0 +1,11 @@
+import {IRootStackProps} from "../../navigator/stack.navigator";
+
+const ChatScreen = ({ navigation }: IRootStackProps<'Chat'>) => {
+	return (
+		<>
+
+		</>
+	)
+}
+
+export default ChatScreen

87040fd7dcfd7d5e52053b066ecc6fcd521b8622
Louis Landois <louis.landois@proton.me>
2023-06-26 09:51:29 +0200

    feat(dependencies): Add ReactNativeScreens and add Override on MainActivity.java

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import StackNavigator from "./src/navigator/stack.navigator";
+import {NavigationContainer} from "@react-navigation/native";
 
 const App = () => {
   return (
     <SafeAreaProvider>
+    <NavigationContainer>
       <StackNavigator/>
+      </NavigationContainer>
     </SafeAreaProvider>
   )
 }

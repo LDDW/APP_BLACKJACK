a1e79b443e65ab4c67ece32740bbce8d5cb185f8
ColinLoic <colinloic@hotmail.com>
2023-06-27 09:16:44 +0200

    Add communication serveur

---
+++ mobile/src/services/FetchRequest.ts
@@ -0,0 +1,53 @@
+/**
+ * FetchRequest
+ * @param route
+ * @param method
+ * @param data
+ * @constructor
+ */
+const FetchRequest = async (
+	route: string,
+	method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE',
+	data: object | null
+) => {
+	let header, datas
+	const token = ''
+
+	if (token) {
+		header = {
+			'Authentication': 'Bearer '+token
+		}
+	}
+
+	/**
+	 * Si une donnée existe, on ajoute la propriété body à fetch
+	 * On transmet la data stringifiée
+	 */
+	if (typeof data === 'object' && data !== null) {
+		datas = {
+			body: JSON.stringify(data)
+		}
+	} else {
+		datas = null
+	}
+
+	try {
+		const req = await fetch(route, {
+			method,
+			headers: {
+				'Content-Type': 'application/json',
+				...header
+			},
+			...datas
+		})
+
+		return await req.json()
+	} catch (err) {
+		if (err instanceof Error) {
+			console.log(err.message)
+			throw new Error(err.message)
+		}
+	}
+}
+
+export default FetchRequest
